21a9953137e93542875b07598428a5c2f232a63b
Yoshinami03 <104010115+minorscarf@users.noreply.github.com>
2026-02-22 13:02:57 +0900

    update: 複数枚のカードに対応させた

--- api.py
+++ api.py
@@ -26,9 +26,10 @@ def read_root():
     return {"Hello": "World"}
 
 @app.post("/cards")
-def post_card(card: CardRequest):
-    if card.suit == Suit.joker and card.value is not None:
-        raise HTTPException(status_code=400, detail="Joker must not have a value")
-    if card.suit != Suit.joker and card.value is None:
-        raise HTTPException(status_code=400, detail="Non-Joker cards must have a value")
-    return {"suit": card.suit, "value": card.value}
+def post_card(cards: list[CardRequest]):
+    for card in cards:
+        if card.suit == Suit.joker and card.value is not None:
+            raise HTTPException(status_code=400, detail="Joker must not have a value")
+        if card.suit != Suit.joker and card.value is None:
+            raise HTTPException(status_code=400, detail="Non-Joker cards must have a value")
+    return [{"suit": card.suit, "value": card.value} for card in cards]
